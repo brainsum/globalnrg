7df36324d9dec61f6753e1078bea1271f52ae85a
Balu Ertl <bertl@brainsum.com>
2019-02-04 14:14:39 +0100

    Merge previous "brainsum/globalnrg.hu" Git repo into current one as a separate branch.

--- web/index.php
+++ web/index.php
@@ -1,22 +1,48 @@
 <?php
 
-/**
- * @file
- * The PHP page that serves all page requests on a Drupal installation.
- *
- * All Drupal code is released under the GNU General Public License.
- * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
- */
 
-use Drupal\Core\DrupalKernel;
-use Symfony\Component\HttpFoundation\Request;
+include("templfunc.php");/**/
 
-$autoloader = require_once 'autoload.php';
+$lang = $_GET['lang'];
+$a = $_GET['a'];
+//----------------------------------------------------file valasztas ---------------
 
-$kernel = new DrupalKernel('prod', $autoloader);
+if (!isset($lang)) $lang="hu";
+if ($lang=="hu") {
+		$ix["lang"]="hu";
+		$ix["lang_other"]="en";
 
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
+		$iix["lang"]="hu";
+		$iix["lang_other"]="en";
+	} else {
+		$ix["lang"]="en";
+		$ix["lang_other"]="hu";
 
-$kernel->terminate($request, $response);
+		$iix["lang"]="en";
+		$iix["lang_other"]="hu";
+	}
+
+if (!isset($a)) $a="main_page";
+
+$filename_array = array (""=>"main_page",
+	"main_page"=>"main_page",
+	"bemutatkozas"=>"bemutatkozas",
+	"elerhetosegek"=>"elerhetosegek",
+	"szolgaltatasok"=>"szolgaltatasok",
+	"hirek"=>"hirek",
+	"cegadatok"=>"cegadatok",
+	"ugyfelszolgalat"=>"ugyfelszolgalat",
+	"uzletszabalyzat"=>"uzletszabalyzat",
+	"dokumentumok"=>"dokumentumok",
+	"2006-evi-beszamolo"=>"2006-evi-beszamolo",
+	"galeria-turaauto2017"=>"galeria-turaauto2017",
+	"tanusitok"=>"tanusitok",
+	"belepes"=>"belepes",
+);
+
+//printf ($iix["lang"]);
+$ix["content"] = parse ($lang."/".$filename_array[$a].".html",$iix,0);
+
+parse("template.html",$ix);
+
+?>
