c9bdda08236d1217864452945f72f1a25e8cf3e2
Peter Ponya <pedro@brainsum.com>
2019-02-22 11:27:22 +0100

    use project specific drush

--- scripts/custom/core.update-prod.sh
+++ scripts/custom/core.update-prod.sh
@@ -3,15 +3,15 @@
 echo "Executing core for update-prod.sh"
 composer install --no-dev -o \
     && cd web \
-    && drush cr \
-    && drush updb -y \
-    && drush cr \
-    && drush cim sync -y \
-    && drush cr \
-    && drush entity-updates -y \
+    && ../vendor/bin/drush cr \
+    && ../vendor/bin/drush updb -y \
+    && ../vendor/bin/drush cr \
+    && ../vendor/bin/drush cim sync -y \
+    && ../vendor/bin/drush cr \
+    && ../vendor/bin/drush entity-updates -y \
     || exit -1
 
-CIMTESTRET=$( { drush cim -n; } 2>&1 )
+CIMTESTRET=$( { ../vendor/bin/drush cim -n; } 2>&1 )
 if [[ ${CIMTESTRET} == "There are no changes to import"* ]]; then
   echo "Config import successful.";
 else
